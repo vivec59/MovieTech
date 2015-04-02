006772624367ee6cf91da9fbab75b7e74eae996c
Camille Dewavrin <camilledewavrin@hotmail.fr>
2015-04-02 15:58:44 +0200

    MAJ CSS

--- index.php
+++ index.php
@@ -9,10 +9,18 @@
 </head>
 
 <body>
+	<div id="header">VIDEOTHEQUE</div>
+	<div id="menu">
+	</div>
 	<div class="main">
 		<?php include_once('view/home.php'); ?>
 	</div>
 
+	<div id="footer">
+		<div id="information">Propriété des développeurs de l'EPSI</div>
+		<div id="propriete">VIDEOTHEQUE @ 2015</div>
+	</div>
+	
 	<script src="public/js/jquery-2.1.3.min.js"></script>
 	<script src="public/js/bootstrap.min.js"></script>
 </body>
