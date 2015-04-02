03f1231326dc18099addd99e38f8c663f27ba82f
Baptiste Rochedo Leca <vivec59@hotmail.fr>
2015-04-02 14:03:58 +0200

    ajout de la base du projet

---
+++ MovieTech/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>MovieTech</title>
+	<link rel="stylesheet" href="style/bootstrap.min.css">
+	<link rel="stylesheet" href="style/main.css">
+	<link rel="stylesheet" href="style/bootstrap.min.css">
+</head>
+
+<body>
+	<div class="main"></div>
+
+	<script src="js/bootstrap.min.js"></script>
+	<script src="jquery-2.1.3.min.js"></script>
+</body>
+</html>
