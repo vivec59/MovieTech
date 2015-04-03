c9c95857cee257707dd9a89c5f1c2e5d0a8bc5f5
Baptiste Rochedo Leca <vivec59@hotmail.fr>
2015-04-03 09:51:55 +0200

    maj script sql

--- ScriptCreationBDD.sql
+++ ScriptCreationBDD.sql
@@ -7,20 +7,6 @@ DROP DATABASE IF EXISTS `movieepsi`;
 
 CREATE DATABASE IF NOT EXISTS `movieepsi`;
 USE `movieepsi`;
--- --------------------------------------------------------
-
---
--- Structure de la table `acteurs`
---
-
-
-CREATE TABLE IF NOT EXISTS `acteurs` (
-  `ID_Film` int(11) NOT NULL,
-  `Nom` varchar(25) DEFAULT NULL,
-  `Prenom` varchar(25) DEFAULT NULL,
-  `Date_Naissance` varchar(25) DEFAULT NULL,
-  PRIMARY KEY (`ID_Film`)
-);
 
 -- --------------------------------------------------------
 
@@ -36,14 +22,3 @@ CREATE TABLE IF NOT EXISTS `film` (
   `Realisateur_Film` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`ID_Film`)
 );
-
---
--- Contraintes pour les tables exportées
---
-
---
--- Contraintes pour la table `film`
---
-ALTER TABLE `film`
-  ADD CONSTRAINT `film_ibfk_1` FOREIGN KEY (`ID_Film`) REFERENCES `acteurs` (`ID_Film`) ON DELETE CASCADE;
-
