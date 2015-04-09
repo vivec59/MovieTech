d71042d28c2a3297238a83dcc921ecfc0c562d8b
Baptiste Rochedo Leca <vivec59@hotmail.fr>
2015-04-09 15:16:20 +0200

    ajout de commentaire dans controller

--- controller/home_controller.php
+++ controller/home_controller.php
@@ -11,9 +11,21 @@ require_once('../allocine-api/PHP/allocine.class.php');
 
 	if (isset($_POST['recherche']))
 	{
+		//appeler la fonction get_movie pour vérifier dans la BDD si le film y existe
+		////////
+		//TODO//
+		////////
+		//si le film n'est pas dans la base, on appelle get_movie_by_name
 		$reponse = json_decode(get_movie_by_name($_POST['recherche']));
-
+		//si il y a plusieurs réponses, on demande de faire un choix à l'utilisateur
+		////////
+		//TODO//
+		////////
 		echo json_encode($reponse);
+		//sinon, on affiche et on ajoute en base (créer fonction add_movie)
+		////////
+		//TODO//
+		////////
 	}
 
 ?>
