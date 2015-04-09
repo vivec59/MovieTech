6c5af7717bb66bab2395db40992b150a30c21167
Camille Dewavrin <camilledewavrin@hotmail.fr>
2015-04-09 15:16:43 +0200

    suppression du +

--- public/js/monScript.js
+++ public/js/monScript.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
                     	$jaquette = "http://fr.web.img4.acsta.net/r_160_240/b_1_d6d6d6/commons/emptymedia/empty_photo.jpg"
                     }
                     var $divfilm = $("<div class='row film'><div class='ListFilm col-md-5 col-md-offset-1'><div class='col-md-2'><img src=\"" + $jaquette + "\" width=\"60\" height=\"80\" /></div> <div class='col-md-10'><label>Nom : </label>  " + value.title + " <label>Titre original : </label> " + value.originalTitle + " </br><label>Année : </label> " + value.productionYear + "</br><label>Acteurs : </label> " + value.castingShort.actors + "</div></div></div>");
-						  
+					
                     $("body").append($divfilm);
 				});
 			}
